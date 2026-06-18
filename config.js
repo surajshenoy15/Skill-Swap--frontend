38ca5e60f0d26400f8c3d5c2099dd06220e2bec0
suraj shenoy <surajshenoyp@gmail.com>
2026-06-19 00:04:56 +0530

    add

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Point this to your running FastAPI backend.
-const API_BASE = "http://localhost:8000";
-const WS_BASE = "ws://localhost:8000";
+const API_BASE = "https://skill-swap-backend-cnqr.onrender.com/";
+const WS_BASE = "https://skill-swap-backend-cnqr.onrender.com/";
